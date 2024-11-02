8e8107ff625225b386458ca527f5ca0d4a1781af
liani250505 <norliani083@gmail.com>
2024-11-02 11:53:05 -0700

    oke

---
+++ bin/oop_pmobile/kode_manipulasi_field.dart
@@ -0,0 +1,9 @@
+void main() {
+  var person = person();
+  person.name = "norliani";
+  person.address = "banjarmasin"
+
+  print(person.name);
+  print(person.address);
+  print(person.country);
+}
