c29050288a34877ff5bf8616f314b5d4146c1313
liani250505 <norliani083@gmail.com>
2024-11-02 11:56:38 -0700

    sudah pa

---
+++ bin/oop_pmobile/kode_tanpa_cascade_notation.dart
@@ -0,0 +1,12 @@
+class User {
+  String? username;
+  String? name;
+  String? email;
+}
+
+void main() {
+  var user = User();
+  user.username = "norliani";
+  user.name = "lia";
+  user.email = "norliani083@example.com";
+}
